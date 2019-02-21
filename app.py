b5193b4d8fb4349fdf0a0622e48d01c1e1cf5b08
Paweł Fertyk <pfertyk@pfertyk.me>
2019-02-22 00:39:53 +0100

    Change port to 8888

--- app.py
+++ app.py
@@ -66,4 +66,4 @@ app.router.add_get('/', get_mars_photo, name='mars_photo')
 
 
 if __name__ == '__main__':
-    web.run_app(app, host='0.0.0.0', port=80)
+    web.run_app(app, host='0.0.0.0', port=8888)
